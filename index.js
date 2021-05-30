7e2cf25b51e91e0b0a6a555ee7a9b084d89c313b
Ana-Carmo <anamtdoc@gmail.com>
2021-05-30 16:51:40 -0300

    Alterações na página de pessoas e esportes.

--- index.js
+++ index.js
@@ -6,29 +6,57 @@ if (typeof Storage === "undefined" || Storage === null) {
     Storage = new JSONStorage('./dados');
 }
 Storage.setItem('pessoas', [{
-        "nome": 'Elaine',
-        "descricao": "qualquer coisa",
-        "imagem": "/stylesheets/img/elaine.jpg"
-    },
-    {
-        "nome": 'Elaine',
-        "descricao": "qualquer coisa",
-        "imagem": "/stylesheets/img/elaine.jpg"
-
-    },
-    {
-        "nome": 'Elaine',
-        "descricao": "qualquer coisa",
-        "imagem": "/stylesheets/img/elaine.jpg"
-
-    },
-    {
-        "nome": 'Elaiane',
-        "descricao": "qualquer coisa",
-        "imagem": "/stylesheets/img/elaine.jpg"
-
-    }
+    "nome": 'Carlos',
+    "descricao": "qualquer coisa",
+    "imagem": "/stylesheets/img/carlos.jpg"
+},
+{
+    "nome": 'Elaine',
+    "descricao": "qualquer coisa",
+    "imagem": "/stylesheets/img/elaine.jpg"
+},
+{
+    "nome": 'Fernanda',
+    "descricao": "qualquer coisa",
+    "imagem": "/stylesheets/img/fernanda.png"
+},
+{
+    "nome": 'Flavia',
+    "descricao": "qualquer coisa",
+    "imagem": "/stylesheets/img/flavia.jpg"
+},
+{
+    "nome": 'Rogério',
+    "descricao": "qualquer coisa",
+    "imagem": "/stylesheets/img/rogerio.jpg"
+}
 ]);
+
+/*if (typeof Storage === "undefined" || Storage === null) { <!-- NÃO ESTA FUNCIONANDO, TEMOS QUE DESCOBRIR O MOTIVO -- ANA -->
+    var JSONStorage = require('node-localstorage').JSONStorage;
+    Storage = new JSONStorage('./dados');
+}
+Storage.setItem('esportes', [{
+    "nome": 'Futebol',
+    "descricao": "Futebol qualquer coisa",
+    "imagem": "/stylesheets/img/futebol.jpg"
+},
+{
+    "nome": 'Futevolei',
+    "descricao": "Futevolei qualquer coisa",
+    "imagem": "/stylesheets/img/futevolei.jpg"
+},
+{
+    "nome": 'Futsal',
+    "descricao": "Futsal qualquer coisa",
+    "imagem": "/stylesheets/img/futsal.jpeg"
+},
+{
+    "nome": 'Vôlei',
+    "descricao": "Vôlei qualquer coisa",
+    "imagem": "/stylesheets/img/volei.png"
+},
+]);*/
 
 express()
     .use(express.static(path.join(__dirname, 'public')))
@@ -36,7 +64,7 @@ express()
     .set('view engine', 'ejs')
     .get('/', (req, res) => res.render('pages/index'))
     .get('/buscaPessoas', (req, res) => res.render('pages/buscaPessoas'))
-    .get('/buscaEquipes', (req, res) => res.render('pages/buscaEquipes'))
+    .get('/buscaEsportes', (req, res) => res.render('pages/buscaEsportes'))
     .get('/buscaCampeonato', (req, res) => res.render('pages/buscaCampeonato'))
     .get('/paginadelogin', (req, res) => res.render('pages/paginadelogin'))
-    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`))
